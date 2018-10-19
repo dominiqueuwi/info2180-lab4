1f57e4c635f0437a1d1db21b3d28eaec7147c930
Dom <dominique_aj@outlook.com>
2018-10-19 09:44:08 -0500

    color

--- maze.js
+++ maze.js
@@ -1,6 +1,3 @@
-maze.js
-<script>
-document.getElementById("boundary1").onclick = function() { 
+document.getElementById("boundary1").onmouseover = function() { 
  document.getElementById("boundary1").style.color= "red";
  }
- </script>
