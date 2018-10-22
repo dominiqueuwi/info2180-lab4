5041883d589a100f82386744ee1a3d471c27c608
Dom <dominique_aj@outlook.com>
2018-10-22 13:53:00 -0500

    all borders

--- maze.js
+++ maze.js
@@ -1,15 +1,34 @@
 
 window.onload = function() { 
-	var x = document.getElementById("start").innerHTML
-	alert(x)
-	alert('Hi')
-	
-	document.getElementById("boundary1").onmouseover = function() { 
-		document.getElementById("boundary1").classList.add("youlose");
+	alert('boundary glow red')
+		var borders; 
+		borders = document.querySelectorAll("boundary");
+		for (var i = 0; i < border.length; ++i) { 
+			border[i].classList.add('youlose')
+		}
 	}
 
 
+	/* document.getElementById("boundary1").onmouseover = function() { 
+		document.getElementById("boundary1").classList.add("youlose"); */
+		 /* var edge=document.querySelectorAll("boundary");
+		for(var x=0;x<edge.length;x++);{ 
+			/*edge[x].classList.add("youlose"); 
+			x[0].style.backgroundColor = "red";
+		}
 
-} 
+	 document.getElementById("boundary").onmouseover = function() { */
+		/* var x = document.querySelectorAll("boundary");
+		var i;
+		for ( i = 0; i < x.length; i++) {
+			document.getElementById("boundary").onmouseover = function() { 
+				document.getElementById("boundary").classList.add("youlose");
+			}
+		} */
+	
+
+
+
+//} 
 
 
