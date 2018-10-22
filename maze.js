68407bbdf91c1530887776325a2c27ca5983a64f
Dom <dominique_aj@outlook.com>
2018-10-22 01:23:05 -0500

    classList add

--- maze.js
+++ maze.js
@@ -1,3 +1,15 @@
-document.getElementById("boundary1").onmouseover = function() { 
- document.getElementById("boundary1").style.color= "red";
- }
+
+window.onload = function() { 
+	var x = document.getElementById("start").innerHTML
+	alert(x)
+	alert('Hi')
+	
+	document.getElementById("boundary1").onmouseover = function() { 
+		document.getElementById("boundary1").classList.add("youlose");
+	}
+
+
+
+} 
+
+
