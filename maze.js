dd9a1cd1bc9b374ac2d41ab26dcf75c6fbb587ba
Dom <dominique_aj@outlook.com>
2018-10-24 10:19:45 -0500

    all boundaries glow red

--- maze.js
+++ maze.js
@@ -1,34 +1,58 @@
 
 window.onload = function() { 
-	alert('boundary glow red')
-		var borders; 
-		borders = document.querySelectorAll("boundary");
-		for (var i = 0; i < border.length; ++i) { 
-			border[i].classList.add('youlose')
-		}
-	}
-
-
-	/* document.getElementById("boundary1").onmouseover = function() { 
-		document.getElementById("boundary1").classList.add("youlose"); */
-		 /* var edge=document.querySelectorAll("boundary");
-		for(var x=0;x<edge.length;x++);{ 
-			/*edge[x].classList.add("youlose"); 
-			x[0].style.backgroundColor = "red";
-		}
-
-	 document.getElementById("boundary").onmouseover = function() { */
-		/* var x = document.querySelectorAll("boundary");
-		var i;
-		for ( i = 0; i < x.length; i++) {
-			document.getElementById("boundary").onmouseover = function() { 
-				document.getElementById("boundary").classList.add("youlose");
-			}
-		} */
 	
 
+	
+ document.getElementById("boundary1").onmouseover = function() { 
+		document.getElementById("boundary1").classList.add("youlose");   	 
+			}
+	
+} /* 
+ document.getElementsByClassName("boundary").onmouseover = function() { 
+ 	loser = true; 
+ 	var boundaries = $$("div#maze div.boundary");
+ 	for (var i = 0; i < boundaries.length; i++) { 
+ 		boundaries[i].addClassName("youlose");
+ 	}
+ } 
+	
+	/* var edge = document.getElementsByClassName("boundary");
+	alert(edge);
+	for (var i = 0; i < edge.length; i++) { 
+	//document.getElementsByClassName("boundary")
+	document.getElementsByClassName("boundary").onmouseover = function () {
+		edge.item(i).style.backgroundColor = "#ff8888"; 
+		} 
+	} 
+  
+
+  alert('done');
 
 
-//} 
+ 	/* var edge = document.getElementsByClassName("boundary");
+ 	alert(edge);
+ 	for (var i = 0; i < edge.length(); i++) {
+ 		alert(i);
+ 		document.getElementsByClassName("boundary").onmouseover = function() { 
+ 		.style.color = "#ff8888";
+ 		}
+ 	
+	}
+*/
 
+	/* var x = document.getElementsByClassName("boundary")[0].className;
+		x.forEach(function(onmouseover)) { 
+			document.getElementsByClassName("boundary").classList.add("youlose"); 
+		}
+	} 
+} */
 
+ /* 
+ document.getElementById("boundary").onmouseover = function() { 
+		var border = document.querySelectorAll("boundary"); 
+		for (var i = 0; i<border.length; i++) {  
+			border[i].classList.add.("youlose");
+		} 
+
+	}
+*/ 
