42b139f31c0a58dab9bdb7312cf135267246d026
Dom <dominique_aj@outlook.com>
2018-10-24 10:21:19 -0500

    start

--- maze.js
+++ maze.js
@@ -7,52 +7,21 @@ window.onload = function() {
 		document.getElementById("boundary1").classList.add("youlose");   	 
 			}
 	
-} /* 
- document.getElementsByClassName("boundary").onmouseover = function() { 
- 	loser = true; 
- 	var boundaries = $$("div#maze div.boundary");
- 	for (var i = 0; i < boundaries.length; i++) { 
- 		boundaries[i].addClassName("youlose");
- 	}
- } 
-	
-	/* var edge = document.getElementsByClassName("boundary");
-	alert(edge);
-	for (var i = 0; i < edge.length; i++) { 
-	//document.getElementsByClassName("boundary")
-	document.getElementsByClassName("boundary").onmouseover = function () {
-		edge.item(i).style.backgroundColor = "#ff8888"; 
-		} 
-	} 
-  
+}; 
 
-  alert('done');
-
-
- 	/* var edge = document.getElementsByClassName("boundary");
- 	alert(edge);
- 	for (var i = 0; i < edge.length(); i++) {
- 		alert(i);
- 		document.getElementsByClassName("boundary").onmouseover = function() { 
- 		.style.color = "#ff8888";
- 		}
- 	
+function overBoundary() { 
+	var boundaries = $$("div#maze div.boundary");
+	for ( var i = 0; i < boundaries.length; i++) { 
+		boundaries[i].style.backgroundColor = "#ff8888"
 	}
-*/
+} 
 
-	/* var x = document.getElementsByClassName("boundary")[0].className;
-		x.forEach(function(onmouseover)) { 
-			document.getElementsByClassName("boundary").classList.add("youlose"); 
-		}
-	} 
-} */
-
- /* 
- document.getElementById("boundary").onmouseover = function() { 
-		var border = document.querySelectorAll("boundary"); 
-		for (var i = 0; i<border.length; i++) {  
-			border[i].classList.add.("youlose");
-		} 
-
+function start() { 
+	loser= true; 
+	var boundaries = $$("div#maze div.boundary"); 
+	for (var i = 0; i < boundaries.length; i++) { 
+		boundaries[i].addClassName("youlose"); 
 	}
-*/ 
+
+
+
